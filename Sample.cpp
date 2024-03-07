e1f9f068c12d8c8ec9bc54a12d01c47658113edf
JustCoolDev <120904934+JustCoolDev@users.noreply.github.com>
2024-03-07 14:20:10 +0100

    Added CLI

--- Sample.cpp
+++ Sample.cpp
@@ -1,23 +1,68 @@
 #include <unistd.h>
+#include <cstdio>
+#include <getopt.h>
 #include "include/ZMemory.h"
+
 using namespace ZMemory;
 
-int main() {
-    pid_t pid;
-    long long base;
-    printf("Waiting for target process...\n");
-    while (1) {
-         pid = find_pid("com.Acessor.Il2CppPOC");
-         if (pid) {
-             printf("Process found, PID: %d\n", pid);
-             base = find_library_base(pid, "libil2cpp.so");
-             ZPatch patch = ZPatch(pid, base+0x524EA4, "00 00 A0 E3 1E FF 2F E1");
-             if (patch.toggle())  
-                  printf("Memory patched sucessfully\n");
-             else 
-                   printf("Failed to patch PID %d\n", pid);
-             return 0;
-         }
-         sleep(1);
+int main(int argc, char* argv[]) {
+    pid_t pid = 0;
+    long long base = 0;
+    bool restore = false;
+    int option;
+    char* processName = nullptr;
+    char* libName = nullptr;
+    char* hexValue = nullptr;
+    long long memoryOffset = 0x0;  // Default memory offset
+
+    while ((option = getopt(argc, argv, "p:l:h:o:r:")) != -1) {
+        switch (option) {
+            case 'p':
+                processName = optarg;
+                break;
+            case 'l':
+                libName = optarg;
+                break;
+            case 'h':
+                hexValue = optarg;
+                break;
+            case 'o':
+                sscanf(optarg, "%llx", &memoryOffset);
+                break;
+            case 'r':
+            	restore = true;
+            	break;
+            default:
+                printf("Usage: %s -p process_name -l library_name -h hex_value -o memory_offset\n", argv[0]);
+                return 1;
+        }
     }
+
+    if (!processName || !libName || !hexValue) {
+        printf("Missing required arguments.\n");
+        printf("Usage: %s -p process_name -l library_name -h hex_value -o memory_offset\n", argv[0]);
+        return 1;
+    }
+
+  //  printf("Waiting for target process...\n");
+
+    while (!(pid = find_pid(processName))) {
+        sleep(1);
+    }
+    
+	while (!(base = find_library_base(pid, libName))) {
+        sleep(1);
+    }
+  	
+    ZPatch patch = ZPatch(pid, base + memoryOffset, hexValue);
+	if(!restore){
+    	if (patch.toggle())
+        	printf("Memory patched successfully\n");
+    	else
+        	printf("Failed to patch PID %d\n", pid);
+	}else{
+		patch.orig_datas();
+		printf("%s", patch.Export_orig_data);
+	}
+    return 0;
 }
